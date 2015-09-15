df2e0cb9b6460d43a3d207ec5198d12f2d9a721c
Jack Riales <jack@thatnaughtypanda.com>
2015-09-14 20:35:30 -0500

    Set new title

--- Tetris.py
+++ Tetris.py
@@ -22,9 +22,9 @@ def main():
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
     BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
-    pygame.display.set_caption('Tetromino')
+    pygame.display.set_caption('Evil Tetris')
 
-    showTextScreen('Tetromino')
+    showTextScreen('Evil Tetris')
     while True: # game loop
         if random.randint(0, 1) == 0:
             pygame.mixer.music.load('assets/tetrisb.mid')
